d6d775b152aa258ae29abf12eaf055adf46863aa
Jesus Bohorquez <jabohorquezc@ufpso.edu.co>
2020-12-22 15:42:54 -0500

    EX. Final AppChats

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:chats/helper/authenticate.dart';
+import 'package:chats/helper/helperfunctions.dart';
+import 'package:chats/views/chatrooms.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool userIsLoggedIn;
+
+  @override
+  void initState() {
+    getLoggedInState();
+    super.initState();
+  }
+
+  getLoggedInState() async {
+    await HelperFunctions.getUserLoggedInSharedPreference().then((value) {
+      setState(() {
+        userIsLoggedIn = value;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlutterChat',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        //primaryColor: Color(0xff145C9E),
+        scaffoldBackgroundColor: Colors.green,
+        //accentColor: Color(0xff007EF4),
+        fontFamily: "OverpassRegular",
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: userIsLoggedIn != null
+          ? userIsLoggedIn ? ChatRoom() : Authenticate()
+          : Container(
+              child: Center(
+                child: Authenticate(),
+              ),
+            ),
+    );
+  }
+}
